96b739a852ab0efdeeb3f3a3cce588638d3c3f81
Aurimas Pabrinkis <aurimas@turfcoach.com>
2024-02-15 12:29:45 +0100

    feat: endpoint security

--- speech/main.py
+++ speech/main.py
@@ -1,10 +1,13 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, Security
 from fastapi.responses import FileResponse
+from fastapi.security import APIKeyHeader
+from fastapi.exceptions import HTTPException
 from transformers import SeamlessM4Tv2Model, AutoProcessor
 import torch
 import torchaudio
 import uvicorn
 import scipy
+import os
 
 app = FastAPI()
 
@@ -15,9 +18,24 @@ model = model.to(device)
 
 SAMPLE_RATE = 16000
 
+api_key_header = APIKeyHeader(name="X-API-Key")
+
+api_key = os.environ.get("API_KEY")
+
+
+def get_api_key(api_key_header: str = Security(api_key_header)) -> str:
+    if api_key_header == api_key:
+        return api_key_header
+    raise HTTPException(
+        status_code=401,
+        detail="Invalid or missing API Key",
+    )
+
 
 @app.post("/translate", response_class=FileResponse)
-async def translate_audio(audio_file: UploadFile = File(...)) -> FileResponse:
+async def translate_audio(
+    api_key: str = Security(get_api_key), audio_file: UploadFile = File(...)
+) -> FileResponse:
     # Save the uploaded audio file
     with open("temp_audio.wav", "wb") as f:
         f.write(await audio_file.read())
